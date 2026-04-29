69432b00d3388411f5d620532ea3a26538c90907
Vasi Breban <vbreban09@gmail.com>
2026-04-29 17:55:40 +0300

    init project react+ts

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="bg-gray-950 text-white min-h-screen">
+            <h1 className="text-4xl text-center pt-20 text-cyan-400">
+                Portfolio coming soon 🚀
+            </h1>
+        </div>
+    )
+}
+
+export default App
